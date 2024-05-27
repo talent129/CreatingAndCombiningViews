4c1ed2d0d032a16b81a33c825ce7d509b2792139
江涛 <e-tao.jiang1@geely.com>
2024-05-27 22:14:20 +0800

    complete

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,29 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Curtis on 2024/5/19.
+//
+
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
